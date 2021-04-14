3ea63e0d7321a8732934d3b0561ee862434db18b
Martin Grönlund <3044707+martingronlund@users.noreply.github.com>
2021-04-14 12:54:53 +0200

    favicon

--- components/meta.js
+++ components/meta.js
@@ -24,12 +24,12 @@ export default function Meta({ title }) {
       <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
+        color="#000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#fff" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#fff" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
